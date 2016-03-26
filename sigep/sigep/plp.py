cecf1dc11a10f2b4d5b95784b4f6369d09f4132b
mstuttgart <michellstut@gmail.com>
2016-03-26 14:57:43 -0300

    [FIX] get_xml()

--- sigep/sigep/plp.py
+++ sigep/sigep/plp.py
@@ -47,7 +47,8 @@ class XmlPLP(TagBase):
         xml += '<versao_arquivo>2.3</versao_arquivo>'
         xml += self.plp.get_xml()
         xml += self.remetente.get_xml()
-
+        xml += self.forma_pagamento.get_xml()
+        
         for obj_post in self.lista_objeto_postal:
             xml += obj_post.get_xml()
 
