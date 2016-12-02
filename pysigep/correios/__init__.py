ce489b08bae45a03fca4b2ee49d19461d5b41f43
martini97 <alessandrofmartini@gmail.com>
2016-12-02 09:49:26 -0200

    Correção na função calcular_preco_prazo

--- pysigep/correios/__init__.py
+++ pysigep/correios/__init__.py
@@ -2,14 +2,10 @@
 # © 2016 Alessandro Fernandes Martini, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import os
 from pysigep import send
-
-TEMPLATES = os.path.join(os.path.join(os.getcwd(), os.pardir))
-TEMPLATES = os.path.join(TEMPLATES, 'templates')
 
 
 def calcular_preco_prazo(**kwargs):
-    TEMPLATE = os.path.join(TEMPLATES, 'CalcularPrecoPrazo.xml')
-    return send(TEMPLATE, 'CalcPrecoPrazoResponse', 'CalcularFretePrazo',
+    path = 'CalcularPrecoPrazo.xml'
+    return send(path, 'CalcPrecoPrazoResponse', 'CalcularFretePrazo',
                 'http://tempuri.org/CalcPrecoPrazo', **kwargs)
