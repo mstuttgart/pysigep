bb31d1d09764a5832fe796d2b916397e1a3991e1
Michel Sabchuk <michel@turbosys.com.br>
2017-01-18 13:40:57 -0200

    Cria testes do verificaDisponibilidadeServicoResponse com mock do requests.

--- test/sigep_test/disponibilidade_servico_test.py
+++ test/sigep_test/disponibilidade_servico_test.py
@@ -27,14 +27,36 @@
 
 from unittest import TestCase
 
+import mock
+from pysigep.exceptions import AmbienteObrigatorioError
 from pysigep.sigep import verifica_disponibilidade_servico
+from pysigep.utils import PRODUCAO, HOMOLOGACAO
+from requests.models import Response
+
+resposta_xml = '''\
+<S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/">
+    <S:Body>
+        <ns2:verificaDisponibilidadeServicoResponse
+        xmlns:ns2="http://cliente.bean.master.sigep.bsb.correios.com.br/">
+            <return>{retorno}</return>
+        </ns2:verificaDisponibilidadeServicoResponse>
+    </S:Body>
+</S:Envelope>'''
+
+
+def fake_requests_post(retorno):
+    def wrap(url, data, headers, verify):
+        response = Response()
+        response.status_code = 200
+        response._content = resposta_xml.format(retorno=retorno)
+        response._content_consumed = True
+        return response
+    return wrap
 
 
 class TestVerificaDisponibilidadeServico(TestCase):
-
-    def test_verifica_disponibilidade_servico(self):
-
-        usuario = {
+    def setUp(self):
+        self.kwargs = {
             'codAdministrativo': '08082650',
             'numeroServico': '40215',
             'cepOrigem': '70002900',
@@ -43,9 +65,25 @@ class TestVerificaDisponibilidadeServico(TestCase):
             'senha': 'n5f9t8',
         }
 
-        with self.assertRaises(Exception):
-            verifica_disponibilidade_servico(**usuario)
+    def test_verifica_disponibilidade_servico_demanda_ambiente(self):
+        with self.assertRaises(AmbienteObrigatorioError):
+            verifica_disponibilidade_servico(**self.kwargs)
 
-        usuario['ambiente'] = 1
-        disponibilidade = verifica_disponibilidade_servico(**usuario)
-        self.assertNotIn('mensagem_erro', disponibilidade)
+    def test_verifica_disponibilidade_servico_resposta_positiva(self):
+        with mock.patch('pysigep.requests.post',
+                        new=fake_requests_post(retorno='true')):
+            self.kwargs['ambiente'] = PRODUCAO
+            retorno = verifica_disponibilidade_servico(**self.kwargs)
+            assert retorno
+
+    def test_verifica_disponibilidade_servico_resposta_negativa(self):
+        with mock.patch('pysigep.requests.post',
+                        new=fake_requests_post(retorno='false')):
+            self.kwargs['ambiente'] = PRODUCAO
+            retorno = verifica_disponibilidade_servico(**self.kwargs)
+            assert not retorno
+
+    def test_verifica_disponibilidade_servico(self):
+        self.kwargs['ambiente'] = HOMOLOGACAO
+        retorno = verifica_disponibilidade_servico(**self.kwargs)
+        self.assertNotIn('mensagem_erro', retorno)
