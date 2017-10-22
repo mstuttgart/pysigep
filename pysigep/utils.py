f50fbd03d320a6372da71c14db072db39e88e8fe
Michell Stuttgart <michellstut@gmail.com>
2017-10-22 14:15:13 -0200

    [NEW] Variaveis para uso de desenvolvimento e homologacao

--- pysigep/utils.py
+++ pysigep/utils.py
@@ -7,3 +7,9 @@ URLS = {
     HOMOLOGACAO: 'https://apphom.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl',  # noqa
     PRODUCAO: 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl',  # noqa
 }
+
+HOMOG_USUARIO = 'sigep'
+HOMOG_SENHA = 'n5f9t8'
+HOMOG_CARTAO = '0067599079'
+HOMOG_CODIGO_ADMIN = '17000190'
+HOMOG_CONTRATO = '9992157880'
