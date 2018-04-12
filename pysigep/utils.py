ae6f9bb8c86023dc24946db81844a449751723ad
mstuttgart <michellstut@gmail.com>
2018-04-12 07:45:55 -0300

    [NEW] Adiciona metodo para calculo local do digito verificador

--- pysigep/utils.py
+++ pysigep/utils.py
@@ -54,9 +54,9 @@ def validar(key, string):
     """Realiza a validação de uma dado texto a partir da regex fornecida.
 
     Arguments:
-        regex {str} -- Expressao regular para ser utilizada na validacao
-        string {str} -- Texto a ser validado
-        msg_erro {Str} -- Mensagem de erro
+        regex {str} -- Expressao regular para ser utilizada na validacao.
+        string {str} -- Texto a ser validado.
+        msg_erro {Str} -- Mensagem de erro.
 
     Raises:
         ValueError -- Quando a string fornecida não coincide com sua expressão regular.
@@ -77,3 +77,44 @@ def trim(string):
     """
 
     return CARACTERES_NUMERICOS.sub('', string)
+
+
+def gera_digito_verificador(etiquetas):
+    """Algoritmo para geração do dígito verificador.
+
+    Arguments:
+        etiquetas {list} -- Lista de etiquetas sem digito verificador.
+
+    Raises:
+        ValueError -- Caso a etiqueta contenha menos de 12 caracteres.
+
+    Returns:
+        list -- lista contendo os digitos verificadores (int).
+    """
+
+    lista_dv = []
+    multiplicadores = [8, 6, 4, 2, 3, 5, 9, 7]
+
+    for etiqueta in etiquetas:
+
+        # Removemos espaco em branco do numero da etiqueta
+        numero = etiqueta[2:10].strip()
+
+        if len(etiqueta) != 13:
+            raise ValueError('Etiqueta %s deve possir tamanho 13. Tamanho encontrado: %d' % (etiqueta,
+                                                                                             len(etiqueta)))
+
+        retorno = numero.zfill(8) if len(numero) < 8 else numero
+
+        resto = sum([int(retorno[i:i + 1]) * multiplicadores[i] for i in range(8)]) % 11
+
+        if resto == 0:
+            dv = 5
+        elif resto == 1:
+            dv = 0
+        else:
+            dv = int(11 - resto)
+
+        lista_dv.append(dv)
+
+    return lista_dv
